61d810196d862163705428aa4f9456ac2d24ff5e
WebbingStudio <mail@webbingstudio.com>
2018-09-28 11:53:51 +0900

    スタイルガイドトップページを.l-containerにする／echoサイトにヘッダ固定効果を追加

---
+++ dist/styleguide/js/common.js
@@ -0,0 +1,10 @@
+(function($){ // start jquery
+$(function(){
+
+    $('.s-header.js-fixnav').EchoFixNav({
+        et: $('.s-header').height()
+    });
+
+}); // end $(function
+})(jQuery); // end jquery
+
